17425218bec374bed13d6e1498fad4cdcee74bd1
bill gates <hannesp@ppedv.de>
2017-02-27 14:32:23 +0100

    ....

--- WindowsApp1/Form2.vb
+++ WindowsApp1/Form2.vb
@@ -21,5 +21,16 @@
             Debug.WriteLine(p.ID)
         Next
 
+        Dim i2 = 12
+        Dim a As String = i2
+
+        Dim z1 = CInt(a)
+        Dim z2 = Convert.ToInt32(a)
+        Dim z3 = CType(a, Integer)
+
+        Dim z4 = String.Format("{0:d5}", i2)
+
+
+
     End Sub
 End Class
