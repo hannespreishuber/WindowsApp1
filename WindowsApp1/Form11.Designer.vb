67777d7155e63b1647e09815954f33d4b1c00822
bill gates <hannesp@ppedv.de>
2017-02-28 16:08:26 +0100

    sql parameter

--- WindowsApp1/Form11.Designer.vb
+++ WindowsApp1/Form11.Designer.vb
@@ -24,6 +24,8 @@ Partial Class Form11
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -45,20 +47,41 @@ Partial Class Form11
         Me.DataGridView1.Size = New System.Drawing.Size(742, 339)
         Me.DataGridView1.TabIndex = 1
         '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(1164, 101)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 31)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(1297, 101)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(153, 31)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'Form11
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1591, 545)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form11"
         Me.Text = "Form11"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Button1 As Button
     Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
 End Class
--- WindowsApp1/Form11.vb
+++ WindowsApp1/Form11.vb
@@ -2,9 +2,15 @@
 
 Public Class Form11
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Dim conn = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\pre\documents\visual studio 2017\Projects\WindowsApp1\WindowsApp1\Database1.mdf"";Integrated Security=True"
         Dim con = New SqlConnection(conn)
-        Dim cmd = New SqlCommand("select * from vornamen", con)
+        Dim cmd = New SqlCommand("select * from vornamen where vorname like '%'+@suche+'%'", con)
+        ' cmd.Parameters.Add(New SqlParameter("suche", TextBox1.Text))
+        cmd.Parameters.AddWithValue("suche", TextBox1.Text)
         Dim liste As New List(Of vorname1)
         con.Open()
         Dim rd = cmd.ExecuteReader
