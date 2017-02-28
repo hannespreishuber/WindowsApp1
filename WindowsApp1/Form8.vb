c18d93d3fca5e5857615e41197db68dac2c9c4e2
bill gates <hannesp@ppedv.de>
2017-02-28 11:37:58 +0100

    Paging und filtern

--- WindowsApp1/Form8.vb
+++ WindowsApp1/Form8.vb
@@ -2,7 +2,7 @@
 
 Public Class Form8
     Dim liste As New List(Of plzort)
-    Dim hliste As New Hashtable()
+    Dim dliste As New Dictionary(Of String, plzort)
     Dim page As Integer = 0
     Private Sub Form8_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
@@ -11,28 +11,39 @@ Public Class Form8
             While rd.Peek > 0
                 Dim daten = rd.ReadLine().Split(";")
                 liste.Add(New plzort With {.plz = daten(0), .ort = daten(1)})
-                'hliste.Add(daten(0), New plzort With {.plz = daten(0), .ort = daten(1)})
+                ' dliste.Add(daten(0), New plzort With {.plz = daten(0), .ort = daten(1)})
+
             End While
         End Using
-
-
-
         DataGridView1.DataSource = liste
     End Sub
 
     Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
 
+
         Label1.Visible = True
         Me.Refresh()
+        Dim watch = System.Diagnostics.Stopwatch.StartNew()
         'Dim suchliste = From o In liste
         '                Order By o.ort
         '                Select New With {.Postleitzahl = o.plz, .Ortschaft = o.ort}
+        'Dim suchliste = From o In liste
+        '                Where (o.plz.Contains(TextBox1.Text))
+        '                Order By o.ort
+        '                Select o
+        'Dim mylist = suchliste.ToList
+
         Dim suchliste = From o In liste
-                        Where (o.plz.Contains(TextBox1.Text))
+                        Where o.plz.Contains(TextBox1.Text)
                         Order By o.ort
                         Select o
-        DataGridView1.DataSource = suchliste.ToList
+
+        Dim mylist = suchliste.ToList
+        watch.Stop()
+
+        DataGridView1.DataSource = mylist.ToList
         Label1.Visible = False
+        Debug.WriteLine(watch.ElapsedMilliseconds)
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
